7d524bd3058aac5afcd64e4b4da2178fc0036bfb
NotTheJerry <quinterodiego208@gmail.com>
2024-03-21 14:05:10 -0600

    Commit inicial ;)

---
+++ src/main/java/com/alibou/example/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.alibou.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean("bean1")
+    public MyFirstClass myFirstBean () {
+        return new MyFirstClass("First bean");
+    }
+
+    @Bean
+    public MyFirstClass mySecondBean () {
+        return new MyFirstClass("Second bean");
+    }
+
+    @Bean
+//    @Primary
+    public MyFirstClass myThirdBean () {
+        return new MyFirstClass("Third bean");
+    }
+}
---
+++ src/main/java/com/alibou/example/ExampleApplication.java
@@ -0,0 +1,20 @@
+package com.alibou.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+
+		var ctx = SpringApplication.run(ExampleApplication.class, args);
+
+
+		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
+		System.out.println(myFirstService.tellAStory());
+		System.out.println(myFirstService.getCustomProperty());
+		System.out.println(myFirstService.getCustomPropertyInt());
+	}
+
+}
