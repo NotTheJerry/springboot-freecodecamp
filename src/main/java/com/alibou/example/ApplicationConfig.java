47e37e2ec1004048e470919b9e6528b1ac43943d
NotTheJerry <quinterodiego208@gmail.com>
2024-03-22 12:07:05 -0600

    Beans y properties/variables de entorno

--- src/main/java/com/alibou/example/ApplicationConfig.java
+++ src/main/java/com/alibou/example/ApplicationConfig.java
@@ -2,8 +2,10 @@ package com.alibou.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class ApplicationConfig {
     @Bean("bean1")
     public MyFirstClass myFirstBean () {
--- src/main/java/com/alibou/example/ExampleApplication.java
+++ src/main/java/com/alibou/example/ExampleApplication.java
@@ -3,12 +3,17 @@ package com.alibou.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collection;
+import java.util.Collections;
+
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
 
-		var ctx = SpringApplication.run(ExampleApplication.class, args);
+		var app = new SpringApplication(ExampleApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "test"));
+		var ctx = app.run(args);
 
 
 		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
