64992f5b77fbd27ce560a0fd47b2181aff5fcc22
NotTheJerry <quinterodiego208@gmail.com>
2024-04-13 20:57:12 -0600

    Finalizando con la introduccion a Spring Data Validation

--- src/main/java/com/alibou/example/student/StudentController.java
+++ src/main/java/com/alibou/example/student/StudentController.java
@@ -1,8 +1,14 @@
 package com.alibou.example.student;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -19,7 +25,7 @@ public class StudentController {
 
     @PostMapping("/students")
     public StudentResponseDto saveStudent(
-            @RequestBody StudentsDto dto
+            @Valid @RequestBody StudentsDto dto
     ) {
         return this.studentService.saveStudent(dto);
     }
@@ -49,5 +55,19 @@ public class StudentController {
             @PathVariable("student-id") Integer id
     ) {
         studentService.delete(id);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException (
+            MethodArgumentNotValidException exp
+    ) {
+        var errors = new HashMap<String, String>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var fieldName = ((FieldError) error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/alibou/example/student/StudentsDto.java
+++ src/main/java/com/alibou/example/student/StudentsDto.java
@@ -1,7 +1,13 @@
 package com.alibou.example.student;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
 public record StudentsDto(
+        @NotEmpty(message = "Firstname should not be empty")
         String firstName,
+        @NotEmpty(message = "Lastname should not be empty")
         String lastName,
         String email,
         Integer schoolId
